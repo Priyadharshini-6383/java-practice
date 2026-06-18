ba588d6eeff653dfc8887d771439794ee5602428
Priyadharshini-6383 <pm.priyadharshini123@gmail.com>
2026-06-18 14:21:11 +0530

    Check the minimum value in the array

--- SearchInRange.java
+++ SearchInRange.java
@@ -1,27 +1,52 @@
-import java.util.*;
+// import java.util.*;
 
 // searching the element in the given range of array
+// public class SearchInRange {
+//     public static void main(String[] args) {
+//         int[] arr = {1 , 2 , 3, 4 ,5 , 6};
+//         int target = 3;
+// int ans = (LinearSearch(arr , target, 1,5));
+//         System.out.println(ans);
+
+//     }
+
+//     static int LinearSearch(int[] arr, int target , int start , int end) {
+
+
+// if(arr.length == 0) {
+//     return -1;
+// }
+
+// for(int index = start ; index <= end ; index++) {
+//     if(arr[index] == target) {
+// return  index;
+//     }
+// }
+// return -1;
+//     }
+// }
+
+// Check the minimum value in the array
+
+import java.util.*;
+
 public class SearchInRange {
     public static void main(String[] args) {
-        int[] arr = {1 , 2 , 3, 4 ,5 , 6};
-        int target = 3;
-int ans = (LinearSearch(arr , target, 1,5));
-        System.out.println(ans);
 
+        int[] arr = {1 , 3 ,5 ,-3 , 2 , 7};
+        System.out.println(min(arr));
     }
 
-    static int LinearSearch(int[] arr, int target , int start , int end) {
+    static int min(int[] arr) {
+        int ans = arr[0];
 
-
-if(arr.length == 0) {
-    return -1;
-}
-
-for(int index = start ; index <= end ; index++) {
-    if(arr[index] == target) {
-return  index;
+        for(int i = 1; i < arr.length ; i++) {
+            if (arr[i] < ans) {
+                ans = arr[i];
+            }
+        }
+        
+return ans;
     }
-}
-return -1;
-    }
+   
 }
