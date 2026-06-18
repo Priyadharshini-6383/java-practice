a7de67b214f052ec87b6686f774d5fd022a2b5b2
Priyadharshini-6383 <pm.priyadharshini123@gmail.com>
2026-06-18 14:22:28 +0530

    search and min value using linear search

--- SearchInRange.java
+++ SearchInRange.java
@@ -1,30 +1,30 @@
-// import java.util.*;
+import java.util.*;
 
 // searching the element in the given range of array
-// public class SearchInRange {
-//     public static void main(String[] args) {
-//         int[] arr = {1 , 2 , 3, 4 ,5 , 6};
-//         int target = 3;
-// int ans = (LinearSearch(arr , target, 1,5));
-//         System.out.println(ans);
+public class SearchInRange {
+    public static void main(String[] args) {
+        int[] arr = {1 , 2 , 3, 4 ,5 , 6};
+        int target = 3;
+int ans = (LinearSearch(arr , target, 1,5));
+        System.out.println(ans);
 
-//     }
+    }
 
-//     static int LinearSearch(int[] arr, int target , int start , int end) {
+    static int LinearSearch(int[] arr, int target , int start , int end) {
 
 
-// if(arr.length == 0) {
-//     return -1;
-// }
+if(arr.length == 0) {
+    return -1;
+}
 
-// for(int index = start ; index <= end ; index++) {
-//     if(arr[index] == target) {
-// return  index;
-//     }
-// }
-// return -1;
-//     }
-// }
+for(int index = start ; index <= end ; index++) {
+    if(arr[index] == target) {
+return  index;
+    }
+}
+return -1;
+    }
+}
 
 // Check the minimum value in the array
 
